490194c14f8a28baec82cd666049fbedcf89eba1
ravi <ravichandra.zonup@gmail.com>
2019-04-04 17:38:11 +0530

    chnages

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Test</title>
+<title>Test1</title>
 </head>
 
 <body>
@@ -8,11 +8,11 @@
 <table border="1" cellpadding="0" cellspacing="0" align="center">
 <tr>
 <td>Name</td>
-<td><input type="text" id="textid" name="text1" value="" onblur="getValid()"/></td></tr>
+<td><input type="text" id="textid" name="text1" value="" onBlur="getValid()"/></td></tr>
 <tr><span id="text_valid"></span>
 <td colspan="2" align="center"><input type="submit" name="submit" value="submit" />
 </table>
 </form>
 </body
-</html>
+></html>
 
