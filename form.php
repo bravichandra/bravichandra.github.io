2cfb83ef2cf0fe01d84f756c4231dd1f115811a8
ravi <ravichandra.zonup@gmail.com>
2019-04-04 17:30:27 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,20 +1,18 @@
-
-<html>
 <head>
-<title>
-
-</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
 </head>
+
 <body>
 <form>
-<table>
+<table border="1" cellpadding="0" cellspacing="0" align="center">
 <tr>
 <td>Name</td>
-<td><input type="text" name="text1" value=""></td></tr>
-<tr>
-<td colspan="2" alilgn="center"><input type="submit" name="submit" value="submit"></td></tr>
+<td><input type="text" id="textid" name="text1" value="" onblur="getValid()"/></td></tr>
+<tr><span id="text_valid"></span>
+<td colspan="2" align="center"><input type="submit" name="submit" value="submit" />
 </table>
 </form>
-</body>
+</body
 </html>
 
